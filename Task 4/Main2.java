571f41eeaad2c271925332d3e387a78ff1d87825
Iffath Sanjida <156676273+bloomingcodeby-iffath@users.noreply.github.com>
2026-06-23 10:03:16 +0600

    Update Main2.java

--- Task 4/Main2.java
+++ Task 4/Main2.java
@@ -1,1 +1,29 @@
+class teammember{
+    protected String name;
+    public teammember(String name){
+        this.name=name;
+            }
+}
+interface BonusEligible{
+    void calculateBonus();
+}
+class Employee extends teammember implements BonusEligible{
+    public Employee(String name){
+        super(name);
+    }
+    public void calculateBonus(){
+        System.out.println("Employee bonus calculated");
+    }
+} 
+class Contrator extends teammember{
+    public Contrator(String name){
+        super(name);
+    }
+}
+public class Main2 {
+    public static void main(String[] args) {
+        BonusEligible emp=new Employee(("Iffath"));
+        emp.calculateBonus();
+    }
+}
 
