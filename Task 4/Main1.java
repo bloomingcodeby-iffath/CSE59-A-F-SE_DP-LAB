b9a3e0cfb692609096ea292c59ebc8c04d498a3a
Iffath Sanjida <156676273+bloomingcodeby-iffath@users.noreply.github.com>
2026-06-23 10:02:47 +0600

    Update Main1.java

--- Task 4/Main1.java
+++ Task 4/Main1.java
@@ -1,1 +1,50 @@
+interface Shape{
+    double calculateArea();
+}
+class Rectangle implements Shape{
+    double length;
+    double width;
+
+    Rectangle(double length, double width){
+        this.length=length;
+        this.width=width;
+    }
+    public double calculateArea(){
+        return length*width;
+    }
+}
+class Circle implements Shape{
+    double radius;
+
+    public Circle(double radius){
+        this.radius=radius;
+    }
+
+    public double calculateArea(){
+        return Math.PI*radius*radius;
+    }
+}
+class AreaCalculator{
+   double calculateTotalArea(Shape[]shapes){
+       double totalArea=0;
+       for(Shape shape : shapes){
+        totalArea+=shape.calculateArea();
+       }
+       return totalArea;
+
+    }
+}
+
+public class Main1 {
+    public static void main(String[] args) {
+        Shape[]  shapes = {
+            new Rectangle(10,4),
+            new Circle(7)
+        };
+
+        AreaCalculator calculator= new AreaCalculator();
+
+        System.out.println(calculator.calculateTotalArea(shapes));
+    }
+}
 
