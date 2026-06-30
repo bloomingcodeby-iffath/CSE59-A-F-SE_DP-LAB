9ef1896b5925e71f5096257f3ad573d9de0dc3c3
Iffath Sanjida <156676273+bloomingcodeby-iffath@users.noreply.github.com>
2026-06-30 11:24:37 +0600

    Implement university member classes and interface
    Added classes for university members and research roles.

--- Task 5/Main5.java
+++ Task 5/Main5.java
@@ -1,1 +1,33 @@
+class UniversityMember{
+    public void attendLecture(){
+        System.out.println("Attending lecture");
+    }
+}
+interface Researcher{
+    void conductResearch();
+}
+class Professor implements Researcher{
+    public void conductResearch(){
+        System.out.println("publishing research");
+    }
+}
+class GraduateResearchAssistant extends UniversityMember implements Researcher{
+    public void conductResearch(){
+        System.out.println("doing research");
+    }
+}
+class UndergraduateStudent extends UniversityMember{
+
+}
+public class Main5 {
+    public static void main(String[] args) {
+        UndergraduateStudent student=new UndergraduateStudent();
+        student.attendLecture();
+        GraduateResearchAssistant gra=new GraduateResearchAssistant();
+        gra.attendLecture();
+        gra.conductResearch();
+        Professor professor=new Professor();
+        professor.conductResearch();
+    }
+}
 
