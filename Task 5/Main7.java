83fcfd719547ce8cf8e5a6e8bed6ecf6480220bc
Iffath Sanjida <156676273+bloomingcodeby-iffath@users.noreply.github.com>
2026-06-30 11:22:47 +0600

    Update Main7.java

--- Task 5/Main7.java
+++ Task 5/Main7.java
@@ -1,1 +1,33 @@
+interface Printer{
+    void printDocument();
+}
+interface Scanner{
+    void scanDocument();
+}
+interface FaxMachine{
+    void faxDocument();
+}
+class MultiFunctionCopier implements Printer, Scanner, FaxMachine{
+    public void printDocument(){
+        System.out.println("Printing...");
+    }
+    public void scanDocument(){
+        System.out.println("Scanning..");
+    }
+    public void faxDocument(){
+        System.out.println("faxing..");
+    }
 
+}
+class BasicPrinter implements Printer{
+    public void printDocument(){
+        System.out.println("printing...");
+    }
+}
+public class Main1 {
+
+     public static void main(String[] args) {
+        BasicPrinter printer = new BasicPrinter();
+        printer.printDocument();
+     }
+}
