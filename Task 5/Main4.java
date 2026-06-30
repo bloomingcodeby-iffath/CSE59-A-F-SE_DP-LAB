37f199e47bc88d3b71b33fa7ac598f8fad2f7038
Iffath Sanjida <156676273+bloomingcodeby-iffath@users.noreply.github.com>
2026-06-30 11:24:15 +0600

    Implement quantization for ResNet18 and MobileNet
    Added OptimizableModel interface and implementations for ResNet18 and MobileNet classes with quantization methods.

--- Task 5/Main4.java
+++ Task 5/Main4.java
@@ -1,1 +1,28 @@
+interface OptimizableModel{
+    void quantize();
+}
+class ResNet18 implements OptimizableModel{
+    public void quantize(){
+        System.out.println("appling int8 quantization");
+    }
+}
+class MobileNet implements OptimizableModel{
+    public void quantize(){
+        System.out.println("appling dynamic quantization");
+    }
+}
+class InterfaceEngine{
+    public void optimize(OptimizableModel model){
+        model.quantize();
+
+    }
+}
+public class Main4 {
+    public static void main(String[] args) {
+        InterfaceEngine engine=new InterfaceEngine();
+    engine.optimize(new ResNet18());
+    engine.optimize(new MobileNet());
+    }
+    
+}
 
