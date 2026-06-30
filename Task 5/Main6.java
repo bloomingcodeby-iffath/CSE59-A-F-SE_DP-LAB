a1b73673015ba431f57a4607db78f6ae40260101
Iffath Sanjida <156676273+bloomingcodeby-iffath@users.noreply.github.com>
2026-06-30 11:25:13 +0600

    Implement HttpService and DatabaseManager interfaces
    Added interfaces for HTTP service, database management, and container operations, along with a WebController implementation.

--- Task 5/Main6.java
+++ Task 5/Main6.java
@@ -1,1 +1,22 @@
+interface HttpService{
+    void handGttpRequest();
+}
+interface DatabaseManager{
+    void executeSqlStatemet();
+}
+interface ContainerOps{
+    void restartDockerConatiner();
+}
+class WebController implements HttpService{
+    public void handGttpRequest(){
+        System.out.println("Routing traffic to endpoint");
+    }
+}
+public class Main6 {
+    public static void main(String[] args) {
+        WebController controller=new WebController();
+        controller.handGttpRequest();
+
+    }
+}
 
